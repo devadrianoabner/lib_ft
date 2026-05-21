b532fd5a0e181a436e2b5f71d3f95fca0fba09a3
Adriano Abner Da Silva Bezerra <adrianda@c3r5p5.42sp.org.br>
2026-05-21 16:18:05 -0300

    Add: memcpm.c

---
+++ ft_memcpm.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcpm.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adrianda <adrianda@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/21 15:05:33 by adrianda          #+#    #+#             */
+/*   Updated: 2026/05/21 15:26:44 by adrianda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	const unsigned char	*s1_ptr;
+	const unsigned char	*s2_ptr;
+	size_t				i;
+
+	s1_ptr = (const unsigned char *) s1;
+	s2_ptr = (const unsigned char *) s2;
+	
+	
+}
