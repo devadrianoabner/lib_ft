9717e9aa7f7cfb5de111cbd4c82d8ac6024babfb
Adriano Abner Da Silva Bezerra <adrianda@c1r7p2.42sp.org.br>
2026-05-25 20:16:57 -0300

    add: ft_memchr.c

---
+++ ft_memchr.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adrianda <adrianda@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/25 17:02:25 by adrianda          #+#    #+#             */
+/*   Updated: 2026/05/25 20:14:05 by adrianda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*memchr(const void *str, int c, size_t n)
+{
+	const unsigned char	*ptr;
+	size_t				i;
+	unsigned char		param;
+
+	ptr = (const unsigned char *) str;
+	param = (unsigned char) c;
+	i = 0;
+	while (n > i)
+	{
+		if (ptr[i] == c)
+			return ((void *) ptr[i]);
+		i++;
+	}
+	return (NULL);
+}
