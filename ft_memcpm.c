5555dc34dab1df0e5fe3596c179bded8e5a316b3
Adriano Abner Da Silva Bezerra <adrianda@c2r8p2.42sp.org.br>
2026-05-25 15:49:41 -0300

    Add: ft_memcpm.c

--- ft_memcpm.c
+++ ft_memcpm.c
@@ -6,7 +6,7 @@
 /*   By: adrianda <adrianda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/21 15:05:33 by adrianda          #+#    #+#             */
-/*   Updated: 2026/05/21 15:26:44 by adrianda         ###   ########.fr       */
+/*   Updated: 2026/05/25 11:50:57 by adrianda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,12 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 
 	s1_ptr = (const unsigned char *) s1;
 	s2_ptr = (const unsigned char *) s2;
-	
-	
+	i = 0;
+	while (n > i)
+	{
+		if (s1_ptr[i] != s2_ptr[i])
+			return (s1_ptr[i] - s2_ptr[i]);
+		i++;
+	}
+	return (0);
 }
