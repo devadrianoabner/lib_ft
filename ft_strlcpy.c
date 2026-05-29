4c3563e93257f4148f2ba9f40dccfdf27103605e
Adriano Abner Da Silva Bezerra <adrianda@c1r4p2.42sp.org.br>
2026-05-28 23:03:24 -0300

    add: start strlcpy

---
+++ ft_strlcpy.c
@@ -0,0 +1,18 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adrianda <adrianda@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/05/28 21:41:37 by adrianda          #+#    #+#             */
+/*   Updated: 2026/05/28 23:01:49 by adrianda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	// Vou construir uma função, onde eu preciso copiar uma string para um buffer e retornar o valor do tamanho da src para que o usuário entenda se houve falha ou não no objetivo
+}
