1d372fa00ef29c07255883866e99b5275d407023
Adriano Abner Da Silva Bezerra <adrianda@c3r6p1.42sp.org.br>
2026-05-29 17:33:00 -0300

    fix: segmentation falt in ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: adrianda <adrianda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/28 20:39:03 by adrianda          #+#    #+#             */
-/*   Updated: 2026/05/29 17:05:53 by adrianda         ###   ########.fr       */
+/*   Updated: 2026/05/29 17:30:52 by adrianda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,8 +17,9 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	size_t	i;
 
 	i = 0;
-	while (i < n - 1 && s1[i] == s2[i])
+	if (n == 0)
+		return (0);
+	while ((i < n - 1 && s1[i] == s2[i]) && s1[i] != 0)
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
-// ft_strncmp("", "", 0): Segmentation fault!
