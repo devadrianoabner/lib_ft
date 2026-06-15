c6d68c5e72f827e3a77608f44b76b6a91a8c626c
Adriano Abner <adriano.abner@live.com>
2026-06-15 16:37:27 -0300

    add: ft_split.c final version

--- ft_calloc.c
+++ ft_calloc.c
@@ -1,9 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adrianda <adrianda@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/15 16:23:15 by adrianda          #+#    #+#             */
+/*   Updated: 2026/06/15 16:35:53 by adrianda         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
 
 void	*ft_calloc(size_t nmemb, size_t size)
 {
-	void *ptr;
+	void	*ptr;
+	size_t	total;
 
+	if (nmemb != 0 && (nmemb * size) / nmemb != size)
+		return (NULL);
+	total = nmemb * size;
 	ptr = malloc(nmemb * size);
 	if (!ptr)
 		return (NULL);
--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: adrianda <adrianda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/14 18:16:38 by adrianda          #+#    #+#             */
-/*   Updated: 2026/06/14 22:34:20 by adrianda         ###   ########.fr       */
+/*   Updated: 2026/06/15 16:21:28 by adrianda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,50 +30,61 @@ static size_t	ft_count_words(char const *s, char c)
 
 static size_t	ft_get_word_len(char const *s, char c)
 {
-	size_t	i;
+	size_t	len;
 
-	i = 0;
-	while (s[i] != 0 && s[i] != c)
-		i++;
-	return (i);
+	len = 0;
+	while (s[len] != 0 && s[len] != c)
+		len++;
+	return (len);
 }
 
-static char	**ft_free_all(char **str, int j)
+static char	**ft_free_all(char **str, size_t i)
 {
-	while (j > 0)
+	size_t	j;
+
+	j = 0;
+	while (j < i)
 	{
 		free(str[j]);
-		j--;
+		j++;
 	}
 	free(str);
 	return (NULL);
 }
 
-char	**ft_split(char const *s, char c)
+static char	**ft_make_split(char const *s, char c, char **str)
 {
 	size_t	i;
-	size_t	j;
 	size_t	len;
+
+	i = 0;
+	while (*s != 0)
+	{
+		if (*s != c)
+		{
+			len = ft_get_word_len(s, c);
+			str[i] = malloc(sizeof(char) * len + 1);
+			if (!str[i])
+				return (ft_free_all(str, i));
+			ft_strlcpy(str[i], s, len + 1);
+			i++;
+			s = s + len;
+		}
+		else
+			s++;
+	}
+	str[i] = NULL;
+	return (str);
+}
+
+char	**ft_split(char const *s, char c)
+{
 	char	**str;
 
-	if (!s)	
+	if (!s)
 		return (NULL);
-	i = 0;
-	j = 0;
 	str = ft_calloc(ft_count_words(s, c) + 1, sizeof(char *));
 	if (!str)
 		return (NULL);
-	while (s[i++] != '\0')
-	{
-		if ((i == 0 || s[i - 1] == c) && s[i] != c)
-		{
-			len = ft_get_word_len(&s[i], c);
-			str[j] = calloc(1, len + 1);
-			if (!str[j])
-				return (ft_free_all(str, j));
-			ft_strlcpy(str[j++], &s[i], len + 1);
-		}
-	}
-	str[j] = NULL;
-	return (str);
+	return (ft_make_split(s, c, str));
 }
